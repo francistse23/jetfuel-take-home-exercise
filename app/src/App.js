ebfd1d3ec2b2d94480d76f2ecf19d7f7fd9dbcb3
francistse23 <wtse.ucsd@gmail.com>
2019-07-02 16:46:11 -0700

    Data rendering. working on styling.

--- app/src/App.js
+++ app/src/App.js
@@ -10,23 +10,78 @@ function App() {
   useEffect(() => {
     fetch(plugCoUrl)
       .then(res => res.json())
-      .then(res => console.log(res))
+      .then(res => setCampaigns(res.campaigns))
       .catch(error => console.log(error));
   }, []);
 
   return (
     <div className="App">
-      Hello
       <div>menuIcon AppIcon</div>
+      {campaigns.length === 0
+        ? "Loading..."
+        : campaigns.map(campaign => Campaign(campaign))}
     </div>
   );
 }
 
 function Campaign(data) {
+  function copyLink() {}
+
   return (
-    <div>
-      <div>
-        <img src={`${data.campaign_icon_url}`} alt="Icon" />
+    <div key={data.id} id={`jetfuel-exercise-${data.id}`}>
+      <div
+        style={{
+          display: "flex",
+          borderBottom: "1px solid gray"
+        }}
+      >
+        <img
+          style={{ maxWidth: "50px", maxHeight: "50px", borderRadius: "10px" }}
+          src={`${data.campaign_icon_url}`}
+          alt="Icon"
+        />
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            textAlign: "left"
+          }}
+        >
+          <h4>{data.campaign_name}</h4>
+          <h4>{data.pay_per_install} per install</h4>
+        </div>
+      </div>
+
+      <div
+        style={{
+          display: "flex",
+          overflowX: "auto",
+          padding: "1rem",
+          backgroundColor: "ghostwhite"
+        }}
+      >
+        {data.medias.map(medium => (
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <img
+              style={{ maxWidth: "75px", maxHeight: "150px" }}
+              src={`${medium.cover_photo_url}`}
+            />
+            <div style={{ display: "flex" }}>
+              <img
+                src={require("./images/link.png")}
+                alt="Copy Link"
+                style={{ maxWidth: "25px", maxHeight: "25px" }}
+              />
+              <a href={`${medium.download_url}`} download>
+                <img
+                  src={require("./images/download.png")}
+                  alt="Download Media"
+                  style={{ maxWidth: "25px", maxHeight: "25px" }}
+                />
+              </a>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
