e5ce411f4a534c73dcb66e6f80dde01f9f7a6b70
francistse23 <wtse.ucsd@gmail.com>
2019-07-03 11:18:21 -0700

    Added simple UI to prompt user to refresh if there's no data and the app is not fetching data. Added inline video playback on play button click.

--- app/src/App.js
+++ app/src/App.js
@@ -6,12 +6,17 @@ const plugCoUrl = "/public/take_home_sample_feed";
 
 function App() {
   const [campaigns, setCampaigns] = useState([]);
+  const [fetching, setFetching] = useState(true);
 
   useEffect(() => {
     fetch(plugCoUrl)
       .then(res => res.json())
-      .then(res => setCampaigns(res.campaigns))
-      .catch(error => console.log(error));
+      .then(res => {
+        setCampaigns(res.campaigns);
+        setFetching(false);
+        console.log(fetching);
+      })
+      .catch(error => console.log(error.message));
   }, []);
 
   return (
@@ -28,6 +33,7 @@ function App() {
           alt="Plug Co"
           style={{ maxWidth: "40px", maxHeight: "40px", margin: "1rem" }}
         />
+        {/* White/Blank spaceholder */}
         <img
           src={require("./images/play.png")}
           style={{ maxWidth: "40px", maxHeight: "40px" }}
@@ -35,26 +41,16 @@ function App() {
       </div>
 
       {/* Campaigns */}
-      {campaigns.length === 0
+      {fetching === true
         ? "Loading..."
-        : campaigns.map(campaign => Campaign(campaign))}
+        : campaigns.length === 0 && fetching === false
+        ? "Uh oh... Something went wrong. Please refresh."
+        : campaigns.map(campaign => <Campaign {...campaign} />)}
     </div>
   );
 }
 
 function Campaign(data) {
-  function copyLink(id) {
-    const area = document.createElement("textarea");
-    const link = document
-      .getElementById(`jetfuel-tracking-link-${id}`)
-      .getAttribute("value");
-    area.value = link;
-    document.body.appendChild(area);
-    area.select();
-    document.execCommand("copy");
-    document.body.removeChild(area);
-  }
-
   return (
     <div key={data.id}>
       {/* Campaign info & pay per install */}
@@ -81,62 +77,103 @@ function Campaign(data) {
       {/* Campaign body */}
       <div className="campaign-body">
         {data.medias.map(medium => (
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              margin: "0.25rem"
-            }}
-          >
-            <div style={{ position: "relative", borderRadius: "5px" }}>
-              <div
-                className={
-                  medium.media_type === "video"
-                    ? "campaign-body-cover-photo video"
-                    : "hidden"
-                }
+          <CampaignVideo data={data} medium={medium} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function CampaignVideo({ data, medium }) {
+  const [playState, setPlayState] = useState("pause");
+
+  function copyLink(id) {
+    const area = document.createElement("textarea");
+    const link = document
+      .getElementById(`jetfuel-tracking-link-${id}`)
+      .getAttribute("value");
+    area.value = link;
+    document.body.appendChild(area);
+    area.select();
+    document.execCommand("copy");
+    document.body.removeChild(area);
+  }
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        margin: "0.25rem"
+      }}
+    >
+      <div style={{ position: "relative", borderRadius: "5px" }}>
+        {/* Play button overlay */}
+        {playState === "pause" ? (
+          <div>
+            <div
+              className={
+                medium.media_type === "video"
+                  ? "campaign-body-cover-photo video"
+                  : "hidden"
+              }
+            >
+              <svg
+                viewBox="-150 -350 500 500"
+                alt="Play video"
+                onClick={() => setPlayState("play")}
               >
-                <svg
-                  // className={medium.media_type !== "video" ? "hidden" : ""}
-                  viewBox="-150 -300 500 500"
-                  alt="Play video"
-                >
-                  <polygon points="70, 55 70, 145 145, 100" fill="#FFF" />
-                </svg>
-              </div>
-              <img
-                className="campaign-body-cover-photo"
-                src={`${medium.cover_photo_url}`}
-              />
+                <polygon points="70, 55 70, 145 145, 100" fill="#FFF" />
+              </svg>
             </div>
 
-            {/* Link & Download Icons */}
-            <div className="campaign-body-icon-menu">
-              <a
-                style={{
-                  width: "50%",
-                  borderRight: "1px solid aliceblue"
-                }}
-                id={`jetfuel-tracking-link-${data.id}`}
-                onClick={() => copyLink(data.id)}
-                value={medium.tracking_link}
-              >
-                <img
-                  className="campaign-body-icon"
-                  src={require("./images/link.png")}
-                  alt={medium.tracking_link}
-                />
-              </a>
-              <a href={medium.download_url} download style={{ width: "50%" }}>
-                <img
-                  className="campaign-body-icon"
-                  src={require("./images/download.png")}
-                  alt="Download Media"
-                />
-              </a>
-            </div>
+            <img
+              className="campaign-body-cover-photo"
+              src={`${medium.cover_photo_url}`}
+              alt="Cover Photo"
+            />
           </div>
-        ))}
+        ) : (
+          <video
+            src={medium.download_url}
+            autoplay
+            controls
+            controlsList="nodownload"
+            muted
+            preload="auto"
+            style={{ width: "100px", height: "180px", borderRadius: "5px" }}
+          />
+        )}
+      </div>
+
+      {/* Link & Download Icons */}
+      <div className="campaign-body-icon-menu">
+        <a
+          style={{
+            width: "50%",
+            borderRight: "1px solid aliceblue"
+          }}
+          id={`jetfuel-tracking-link-${data.id}`}
+          onClick={() => copyLink(data.id)}
+          value={medium.tracking_link}
+        >
+          <img
+            className="campaign-body-icon"
+            src={require("./images/link.png")}
+            alt={medium.tracking_link}
+          />
+        </a>
+        <a
+          href={medium.download_url}
+          download={`${data.campaign_name}-${data.id}`}
+          style={{ width: "50%" }}
+        >
+          <img
+            className="campaign-body-icon"
+            src={require("./images/download.png")}
+            alt="Download Media"
+          />
+        </a>
       </div>
     </div>
   );
